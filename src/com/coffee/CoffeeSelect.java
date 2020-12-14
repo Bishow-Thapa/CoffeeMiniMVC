de13c08e048bda4ba492d36c41e4cbfe7ae3f0c5
Bishow Thapa <bishowthapa33@gmail.com>
2020-12-14 12:25:11 +0545

    Added results.jsp file, CoffeeExpert.java model file & some changes

--- src/com/coffee/CoffeeSelect.java
+++ src/com/coffee/CoffeeSelect.java
@@ -2,21 +2,33 @@ package com.coffee;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.coffee.model.*; 
 
 public class CoffeeSelect extends HttpServlet 
 {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("My Coffee Selection <br>");
-		
 		String c = request.getParameter("coffee");
-		out.println("<br> I would like to drink " + c + " Coffee.");
+		
+		CoffeeExpert ce = new CoffeeExpert();
+		List result = ce.getCoffee(c);
+		
+		//response.setContentType("text/html");
+		//PrintWriter out = response.getWriter();
+		//out.println("My Coffee Selection <br>");
+		
+		request.setAttribute("styles", result);
+		
+		RequestDispatcher view = request.getRequestDispatcher("results.jsp");
+		
+		view.forward(request, response);
 	}
 }
